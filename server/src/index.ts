a2636c28d61ae2d77199b0021ed99d82a43b2f81
Erwan Guillet <erwan.guillet@decathlon.com>
2023-01-11 09:24:16 +0100

    :memo: add log on start server to display url of apollo server

--- server/src/index.ts
+++ server/src/index.ts
@@ -41,4 +41,5 @@ app.use(
 );
 
 await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
-console.log(`🚀 Server ready at http://localhost:4000/`);
+console.log(`🚀 Express server ready at http://localhost:4000/`);
+console.log(`🚀 Apollo server ready at http://localhost:4000/graphql`);
